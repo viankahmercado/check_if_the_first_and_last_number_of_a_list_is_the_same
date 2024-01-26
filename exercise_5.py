838bcf0a8316b05faa9a0db231c62e903bc2ff76
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 20:12:11 +0800

    assign the list

--- exercise_5.py
+++ exercise_5.py
@@ -4,9 +4,10 @@
 
 # Exercise 5:
 # Check if the first and last number of a list is the same
-
 # Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
 
 # use a function and number list
 
 # assign the two list and check by printing the result
+first_list = [10, 20, 30, 40, 10]
+second_list = [75, 65, 35, 75, 30]
