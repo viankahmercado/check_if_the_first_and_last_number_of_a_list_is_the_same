73c96d98c97e1ab2cd976e0b75ebaf2744d36473
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 19:54:06 +0800

    description

---
+++ exercise_5.py
@@ -0,0 +1,8 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 5:
+# Check if the first and last number of a list is the same
+
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
